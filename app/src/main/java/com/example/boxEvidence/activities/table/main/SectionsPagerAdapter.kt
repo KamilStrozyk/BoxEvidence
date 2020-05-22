011bf990fee3188e43433dc9c6a50104a6bdf268
Kamil Stróżyk <strozykkamil@gmail.com>
2020-05-22 14:27:34 +0200

    Add blank Location fragment

--- app/src/main/java/com/example/boxEvidence/activities/table/main/SectionsPagerAdapter.kt
+++ app/src/main/java/com/example/boxEvidence/activities/table/main/SectionsPagerAdapter.kt
@@ -4,11 +4,12 @@ import android.content.Context
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
-import com.example.boxEvidence.R
+
 
 private val TAB_TITLES = arrayOf(
-    R.string.tab_text_1,
-    R.string.tab_text_2
+    "Locations",
+    "Boxes",
+    "Items"
 )
 
 /**
@@ -21,15 +22,22 @@ class SectionsPagerAdapter(private val context: Context, fm: FragmentManager) :
     override fun getItem(position: Int): Fragment {
         // getItem is called to instantiate the fragment for the given page.
         // Return a PlaceholderFragment (defined as a static inner class below).
+        when (position) {
+            0 -> return LocationView()
+            1 -> return PlaceholderFragment.newInstance(position + 1)
+            2 -> return PlaceholderFragment.newInstance(position + 1)
+        }
         return PlaceholderFragment.newInstance(position + 1)
     }
 
+
+
     override fun getPageTitle(position: Int): CharSequence? {
-        return context.resources.getString(TAB_TITLES[position])
+        return TAB_TITLES[position]
     }
 
     override fun getCount(): Int {
-        // Show 2 total pages.
-        return 2
+
+        return 3
     }
 }
